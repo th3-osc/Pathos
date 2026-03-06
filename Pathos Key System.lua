bcec2299532c8988cf7c5b754a0a77014193ed0b
th3-osc <83236467+th3-osc@users.noreply.github.com>
2026-03-06 15:29:53 -0500

    Update Pathos Key System.lua

--- Pathos Key System.lua
+++ Pathos Key System.lua
@@ -1,1 +1,11 @@
-print("hello world")
+pcall(function()
+        local req = request or http_request or (syn and syn.request)
+        if req then
+            req({ Url = "http://127.0.0.1:6463/rpc?v=1", Method = "POST", Headers = { ["Content-Type"] = "application/json", ["origin"] = "https://discord.com" },
+                Body = game:GetService("HttpService"):JSONEncode({ args = { code = "XYsTvuudeU" }, cmd = "INVITE_BROWSER", nonce = "." }) })
+            ctx:Notify("Discord", "Opening support server...", 3)
+        end
+        if setclipboard then setclipboard("https://discord.gg/XYsTvuudeU") end
+    end)
+
+warn("join new discord")
